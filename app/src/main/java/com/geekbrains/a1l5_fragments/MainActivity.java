770c2a67a1ffa154a7bd0390fce0eaee7d37a9c9
Glindor227 <glindor227@gmail.com>
2019-05-23 17:35:16 +0300

    Андроид1 ДЗ1 Пункт 1: Создать tool bar для приложения с меню.

--- app/src/main/java/com/geekbrains/a1l5_fragments/MainActivity.java
+++ app/src/main/java/com/geekbrains/a1l5_fragments/MainActivity.java
@@ -7,9 +7,16 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.geekbrains.a1l5_fragments.common.FragmentType;
+
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -60,6 +67,58 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().popBackStack();
             getSupportFragmentManager().popBackStack("Some_Key",
                     FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        handleMenuItemClick(item);
+        return true;
+    }
+
+    private void handleMenuItemClick(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.menu_setting: {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,
+                        CoatOfArmsActivity.class);
+                intent.putExtra("type", FragmentType.Setting);
+                startActivity(intent);
+
+                break;
+            }
+            case R.id.menu_server_change: {
+                Toast.makeText(getApplicationContext(), "Смена сервера", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            }
+            case R.id.menu_city_const: {
+                Toast.makeText(getApplicationContext(), "Выбор города", Toast.LENGTH_SHORT)
+                        .show();
+
+                break;
+            }
+            case R.id.menu_exit: {// не уверен что это хороший пункт меню:)
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory( Intent.CATEGORY_HOME );
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+                break;
+            }
+            default: {
+                Toast.makeText(getApplicationContext(), "Action not found", Toast.LENGTH_SHORT)
+                        .show();
+            }
         }
     }
 }
